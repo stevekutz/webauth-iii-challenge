8eaafea965d8e806f9a1799016b14aa67d0e400f
stevekutz <stkutz@gmail.com>
2019-06-27 22:10:09 -0500

    can render out users in console after logging in

--- client/src/Users.js
+++ client/src/Users.js
@@ -1,7 +1,21 @@
 import React from 'react';
 import { Fragment } from 'react';
+import api from './helpers/api';
 
 class Users extends React.Component {
+    
+    async componentDidMount() {
+        try {
+            const result = await api.get('/users');
+            console.log(result);
+        } catch (err) {
+            console.error(err);
+        }
+
+    }
+    
+    
+    
     render() {
         return (
             <Fragment>
