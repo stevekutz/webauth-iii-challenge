6b052c76a2d82900c582b7f7a5467e85359308e3
stevekutz <stkutz@gmail.com>
2019-06-27 23:18:51 -0500

    added logout functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,48 @@
 import React from 'react';
+import { Fragment } from 'react';
 // import logo from './logo.svg';
 // import './App.css';
 
-import {Route, NavLink} from 'react-router-dom';   // ADD HERE for route fun!
+//import {Route, NavLink} from 'react-router-dom';   // ADD HERE for route fun!
+
+import {Route, NavLink, withRouter} from 'react-router-dom';   // ADD HERE for route fun!
 import SignUp from './SignUp';
 import SignIn from './SignIn';
 import Users from './Users';
 
 
-function App() {
-  const logout = () => {
-    localStorage.removeItem('token')
+//function App() {
+class App extends React.Component {
+
+  logout = () => {
+    localStorage.removeItem('jwt')
     this.props.history.push('/login')
   }
 
-  return (
-    <>
-      <h2> Authentication with JWT-iv >> FullStack !</h2>
 
-      <ul>
-        <li><NavLink to = "/signup">Signup</NavLink></li>
-        <li><NavLink to = "/login">Login</NavLink></li>
-        <li><NavLink to = "/users">Users </NavLink></li>
-        <li><button onClick={logout}>Logout</button></li>
-      
-      </ul>
+  render() {
+    return (
+      <Fragment>
+        <h2> Authentication with JWT-iv >> FullStack !</h2>
+
+        <ul>
+          <li><NavLink to = "/signup">Signup</NavLink></li>
+          <li><NavLink to = "/login">Login</NavLink></li>
+          <li><NavLink to = "/users">Users </NavLink></li>
+          <li><button onClick={this.logout}>Logout</button></li>
+        
+        </ul>
 
 
-      <main>
-        <Route path = "/signup" component = {SignUp}/>
-        <Route path = "/login" component = {SignIn} />
-        <Route path = "/users" component = {Users}/>
-      </main>
-    </>
-  );
+        <main>
+          <Route path = "/signup" component = {SignUp}/>
+          <Route path = "/login" component = {SignIn} />
+          <Route path = "/users" component = {Users}/>
+        </main>
+      </Fragment>
+    );
+  }  
 }
 
-export default App;
+//export default App;
+export default withRouter(App);
